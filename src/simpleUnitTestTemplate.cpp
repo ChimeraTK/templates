f0ad3be7f28256c7938eee8faa41edebefe1cbc4
Martin Hierholzer <martin.hierholzer@desy.de>
2019-02-21 18:26:07 +0100

    changed formatting

--- src/simpleUnitTestTemplate.cpp
+++ src/simpleUnitTestTemplate.cpp
@@ -13,34 +13,34 @@ using namespace boost::unit_test_framework;
 // #include "theStuffIWantToTest.h"
 
 // This just thows the int -1 if you put true as argument.
-void throwIfTrue( bool doThrow ){
-  if (doThrow){
-    throw -1;
+void throwIfTrue(bool doThrow) {
+  if (doThrow) {
+    throw - 1;
   }
 }
 
 // Create a test suite which holds all your tests.
-BOOST_AUTO_TEST_SUITE( SimpleExampleTestSuite )
+BOOST_AUTO_TEST_SUITE(SimpleExampleTestSuite)
 
 // Define one test after another. Each one needs a unique name.
-BOOST_AUTO_TEST_CASE( testSomethingSimple ){
-  int a=3;
+BOOST_AUTO_TEST_CASE(testSomethingSimple) {
+  int a = 3;
   // Use BOOST_CHECK to test conditions. They all just have to be true.
-  BOOST_CHECK( a == 3 );
-  BOOST_CHECK( true );
+  BOOST_CHECK(a == 3);
+  BOOST_CHECK(true);
 
   // If you uncomment this the test will fail.
   //  BOOST_CHECK( 7 > 15 );
 }
 
-BOOST_AUTO_TEST_CASE( testThrowing ){
+BOOST_AUTO_TEST_CASE(testThrowing) {
   // Give the type of exception which has to be thrown.
-  BOOST_CHECK_THROW( throwIfTrue(true), int );
+  BOOST_CHECK_THROW(throwIfTrue(true), int);
 
   // Another example which would fail.
   //  BOOST_CHECK_NO_THROW( throwIfTrue(true) );
 
-  BOOST_CHECK_NO_THROW( throwIfTrue(false) );
+  BOOST_CHECK_NO_THROW(throwIfTrue(false));
 }
 
 // After you finished all test you have to end the test suite.
--- src/unitTestTemplate.cpp
+++ src/unitTestTemplate.cpp
@@ -23,30 +23,30 @@
 #include <boost/test/included/unit_test.hpp>
 using namespace boost::unit_test_framework;
 
-class ExampleException{
-};
+class ExampleException {};
 
-class ExampleClass{
+class ExampleClass {
 public:
   static const int INITIAL_VALUE = 4711;
 
-  ExampleClass(bool someCondition=true) : _a(INITIAL_VALUE) {
-    if (someCondition==false){throw ExampleException();}
+  ExampleClass(bool someCondition = true) : _a(INITIAL_VALUE) {
+    if (someCondition == false) {
+      throw ExampleException();
+    }
   }
-  int read(){return _a;}
-  void write(int a){_a=a;}
+  int read() { return _a; }
+  void write(int a) { _a = a; }
 
 private:
   int _a;
 };
 
-/** The unit tests for the class under test (A in this case), based on the 
- *  boost unit test library. We use a class which holds a private 
+/** The unit tests for the class under test (A in this case), based on the
+ *  boost unit test library. We use a class which holds a private
  *  instance of A, which avoids code duplication.
  */
-class ExampleClassTest
-{
- public:
+class ExampleClassTest {
+public:
   /** The constructor test is static. It does not use the internal variable.
    *  Needed to check if the constructor does thow (if it can throw).
    *  There might be other cases for static tests.
@@ -55,71 +55,76 @@ class ExampleClassTest
 
   void testRead();
   void testWrite();
-  void testSomethingElse();// could also be static, but is not for demonstation
+  void testSomethingElse(); // could also be static, but is not for demonstation
 
- private:
+private:
   ExampleClass _exampleClass;
 };
 
 class ExampleClassTestSuite : public test_suite {
 public:
-  ExampleClassTestSuite(): test_suite("ExampleClass test suite") {
+  ExampleClassTestSuite() : test_suite("ExampleClass test suite") {
     // create an instance of the test class
-    boost::shared_ptr<ExampleClassTest> exampleClassTest( new ExampleClassTest );
+    boost::shared_ptr<ExampleClassTest> exampleClassTest(new ExampleClassTest);
 
     // add the tests
     // static tests are added with BOOST_TEST_CASE
-    add( BOOST_TEST_CASE( &ExampleClassTest::testConstructor ) );
-    // tests of non-static functions are added using BOOST_CLASS_TEST_CASE, which needs a (shared) pointer to the instance
-    add( BOOST_CLASS_TEST_CASE( &ExampleClassTest::testSomethingElse, exampleClassTest ) );
+    add(BOOST_TEST_CASE(&ExampleClassTest::testConstructor));
+    // tests of non-static functions are added using BOOST_CLASS_TEST_CASE,
+    // which needs a (shared) pointer to the instance
+    add(BOOST_CLASS_TEST_CASE(&ExampleClassTest::testSomethingElse,
+                              exampleClassTest));
 
-    // in case of dependencies store the test cases before adding them and declare the dependency
-    test_case* writeTestCase = BOOST_CLASS_TEST_CASE( &ExampleClassTest::testWrite, exampleClassTest );
-    test_case* readTestCase = BOOST_CLASS_TEST_CASE( &ExampleClassTest::testRead, exampleClassTest );
+    // in case of dependencies store the test cases before adding them and
+    // declare the dependency
+    test_case *writeTestCase =
+        BOOST_CLASS_TEST_CASE(&ExampleClassTest::testWrite, exampleClassTest);
+    test_case *readTestCase =
+        BOOST_CLASS_TEST_CASE(&ExampleClassTest::testRead, exampleClassTest);
 
-    // the write test uses read, so we first have to check that reading works before we know that writing also does
+    // the write test uses read, so we first have to check that reading works
+    // before we know that writing also does
     writeTestCase->depends_on(readTestCase);
 
-    add( readTestCase );
-    add( writeTestCase );
+    add(readTestCase);
+    add(writeTestCase);
   }
 };
 
-test_suite*
-init_unit_test_suite( int /*argc*/, char* /*argv*/ [] )
-{
+test_suite *init_unit_test_suite(int /*argc*/, char * /*argv*/ []) {
   framework::master_test_suite().p_name.value = "ExampleClass test suite";
   return new ExampleClassTestSuite;
 }
 
-void ExampleClassTest::testConstructor(){
+void ExampleClassTest::testConstructor() {
   // in case an exception can be thrown, always check that this happens when it
   // has to, and that it does not happen when it should not happen.
-  BOOST_CHECK_THROW( ExampleClass(false), ExampleException );
-  BOOST_CHECK_NO_THROW( ExampleClass(true) );
+  BOOST_CHECK_THROW(ExampleClass(false), ExampleException);
+  BOOST_CHECK_NO_THROW(ExampleClass(true));
 }
 
-void ExampleClassTest::testSomethingElse(){
+void ExampleClassTest::testSomethingElse() {
   int value = 12 / 4;
-  BOOST_CHECK( value  == 3 );
+  BOOST_CHECK(value == 3);
 }
 
-void ExampleClassTest::testRead(){
+void ExampleClassTest::testRead() {
   // check for value as set by the constructor
-  BOOST_CHECK( _exampleClass.read() == ExampleClass::INITIAL_VALUE );
+  BOOST_CHECK(_exampleClass.read() == ExampleClass::INITIAL_VALUE);
 }
 
-void ExampleClassTest::testWrite(){
+void ExampleClassTest::testWrite() {
   // typical three step test
   // 1) prepare
-  //    here we create a value which we know is different from the current 
+  //    here we create a value which we know is different from the current
   //    content to see if writing really changed something
-  int value =  _exampleClass.read() + 1;
+  int value = _exampleClass.read() + 1;
 
   // 2) execute
-  _exampleClass.write( value );
+  _exampleClass.write(value);
 
   // 3) check
-  // we use read to check that the value was written. We checked before that read acutally works.
-  BOOST_CHECK( _exampleClass.read() == value); 
+  // we use read to check that the value was written. We checked before that
+  // read acutally works.
+  BOOST_CHECK(_exampleClass.read() == value);
 }
